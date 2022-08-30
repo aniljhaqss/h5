548818b0c3c7a8b9659d8520e0a617645208237c
aniljha <anil.jha@qsstechnosoft.com>
2022-08-30 17:26:06 +0530

    commiting hello2 and One2 to branchx

---
+++ src/h5/One2.java
@@ -0,0 +1,12 @@
+package h5;
+public class One2
+{
+	public static void main(String[] args) 
+	{
+	 int num1=10;
+	 int num2 =12;
+	 int num4 =14;
+	 int num3=num1*num2*num4;
+	System.out.println(num3);
+	}
+}
